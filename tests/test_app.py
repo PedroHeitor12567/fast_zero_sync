f95d3af0c36eaf2ce5d402c12818381c4fd0108d
PedroHeitor12567 <pedroheitorrdac@gmail.com>
2025-11-05 08:27:55 -0300

    Arquivos config

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,26 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from projeto_fastapi.app import app
-
-
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
-
+def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK  # 200
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'testusername',
+            'email': 'test@test.com',
+            'password': 'testpassword',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'testusername',
+        'email': 'test@test.com',
+        'id': 1,
+    }
